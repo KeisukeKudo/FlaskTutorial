3a16ba4e6a550e64c4947cf38fe17d4841df50fc
KeisukeKudo <melchior.balthasar.casper326@gmail.com>
2017-11-17 16:02:03 +0900

    all complete.

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,6 +1,25 @@
-from flask import request, redirect, url_for, render_template, flash, abort, jsonify
+from flask import request, redirect, url_for, render_template,\
+                  flash, abort, jsonify, session, g
 from flaskr import app, db
 from flaskr.models import Entry, User
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.path))
+        return f(*args, **kwargs)
+    return decorated_view
+
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(session['user_id'])
 
 
 @app.route('/')
@@ -19,18 +38,21 @@ def add_entry():
 
 
 @app.route('/users/')
+@login_required
 def user_list():
     users = User.query.all()
     return render_template('user/list.html', users=users)
 
 
 @app.route('/users/<int:user_id>/')
+@login_required
 def user_detail(user_id):
     user = User.query.get(user_id)
     return render_template('user/detail.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+@login_required
 def user_edit(user_id):
     user = User.query.get(user_id)
     if user is None:
@@ -47,6 +69,7 @@ def user_edit(user_id):
 
 
 @app.route('/users/create', methods=['GET', 'POST'])
+@login_required
 def user_create():
     if request.method == 'POST':
         user = User(name=request.form['name'],
@@ -69,3 +92,26 @@ def user_delete(user_id):
     db.session.delete(user)
     db.session.commit()
     return jsonify({'status': 'OK'})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user, authenticated =\
+            User.authenticate(db.session.query, request.form['email'], request.form['password'])
+
+        if authenticated:
+            session['user_id'] = user.id
+            flash('You wer logged in')
+            return redirect(url_for('show_entries'))
+
+        flash('Invalid email or password')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
